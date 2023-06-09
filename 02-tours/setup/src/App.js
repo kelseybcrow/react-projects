9a8c8442bdc2c68b55b00d67da77254de9202ebe
kelseybcrow <kelseybcrow@gmail.com>
2023-06-09 11:36:37 +0200

    finished tours

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -9,6 +9,11 @@ function App() {
   const [loading, setLoading] = useState(true)
   const [tours, setTours] = useState([])
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
+
   const fetchTours = async () => {
     setLoading(true)
 
@@ -22,6 +27,7 @@ function App() {
       console.log(error)
     }
   }
+
   useEffect(() => {
     fetchTours()
   }, [])
@@ -33,9 +39,23 @@ function App() {
       </main>
     )
   }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={fetchTours}>
+            refresh
+          </button>
+        </div>
+      </main>
+    )
+  }
+
   return (
     <main>
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   )
 }
