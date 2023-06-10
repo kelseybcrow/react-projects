d1cc58f0d09fe75e4fe3f0c4d16617e8f43be461
kelseybcrow <kelseybcrow@gmail.com>
2023-06-10 21:24:30 +0200

    finished menu

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import data from './data'
 import SingleQuestion from './Question'
+
 function App() {
   const [questions, setQuestions] = [useState(data)]
+
   return (
     <main>
       <div className='container'>
